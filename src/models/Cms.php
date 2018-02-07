73bff0abfb61673382407280ac72be225d1b2e4b
usama khalil <usama.khalil@optimageeks.com>
2018-02-07 15:23:31 +0500

    added funtions to get page and post types

--- src/models/Cms.php
+++ src/models/Cms.php
@@ -34,13 +34,9 @@ class Cms extends Model
     {
         $webroot = Yii::getAlias('@webroot');
         if (!is_dir($webroot . '/uploads/'))
-        {
             mkdir($webroot . '/uploads/');
-        }
         if (!is_dir($webroot . '/uploads/temp/'))
-        {
             mkdir($webroot . '/uploads/temp/');
-        }
         $file = $webroot . '/uploads/temp/settings.json';
         if (!file_exists($file) || (file_exists($file) && time() - filemtime($file) > 2 * 3600))
         {
@@ -58,13 +54,9 @@ class Cms extends Model
     {
         $webroot = Yii::getAlias('@webroot');
         if (!is_dir($webroot . '/uploads/'))
-        {
             mkdir($webroot . '/uploads/');
-        }
         if (!is_dir($webroot . '/uploads/temp/'))
-        {
             mkdir($webroot . '/uploads/temp/');
-        }
         $file = $webroot . '/uploads/temp/menu.json';
         if (!file_exists($file) || (file_exists($file) && time() - filemtime($file) > 2 * 3600))
         {
@@ -82,17 +74,53 @@ class Cms extends Model
     {
         $webroot = Yii::getAlias('@webroot');
         if (!is_dir($webroot . '/uploads/'))
-        {
             mkdir($webroot . '/uploads/');
-        }
         if (!is_dir($webroot . '/uploads/temp/'))
-        {
             mkdir($webroot . '/uploads/temp/');
-        }
         $file = $webroot . '/uploads/temp/languages.json';
         if (!file_exists($file) || (file_exists($file) && time() - filemtime($file) > 2 * 3600))
         {
             $file_data = file_get_contents(Yii::$app->params['apiUrl'] . 'cms/languages&user=' . Yii::$app->params['user'] . '&name=gogo');
+            file_put_contents($file, $file_data);
+        }
+        else
+        {
+            $file_data = file_get_contents($file);
+        }
+        return json_decode($file_data, TRUE);
+    }
+
+    public static function pageBySlug($slug)
+    {
+        $webroot = Yii::getAlias('@webroot');
+        if (!is_dir($webroot . '/uploads/'))
+            mkdir($webroot . '/uploads/');
+        if (!is_dir($webroot . '/uploads/temp/'))
+            mkdir($webroot . '/uploads/temp/');
+        $file = $webroot . '/uploads/temp/' . $slug . '.json';
+        if (!file_exists($file) || (file_exists($file) && time() - filemtime($file) > 2 * 3600))
+        {
+            $file_data = file_get_contents(Yii::$app->params['apiUrl'] . 'cms/page-by-slug&user=' . Yii::$app->params['user'] . '&slug=' . $slug);
+            file_put_contents($file, $file_data);
+        }
+        else
+        {
+            $file_data = file_get_contents($file);
+        }
+        return json_decode($file_data, TRUE);
+    }
+
+    public static function postTypes($name)
+    {
+        $webroot = Yii::getAlias('@webroot');
+        if (!is_dir($webroot . '/uploads/'))
+            mkdir($webroot . '/uploads/');
+        if (!is_dir($webroot . '/uploads/temp/'))
+            mkdir($webroot . '/uploads/temp/');
+        $file = $webroot . '/uploads/temp/' . str_replace(' ', '_', strtolower($name)) . '.json';
+        if (!file_exists($file) || (file_exists($file) && time() - filemtime($file) > 2 * 3600))
+        {
+            $file_data = file_get_contents(Yii::$app->params['apiUrl'] . 'cms/posts&user=' . Yii::$app->params['user'] . '&post_type=' . $name);
             file_put_contents($file, $file_data);
         }
         else
