73612ba8017f614a394fbd3856b7f2b3150dec79
usama khalil <usama.khalil@optimageeks.com>
2018-02-10 12:52:46 +0500

    contect us form conected

--- src/models/ContactUs.php
+++ src/models/ContactUs.php
@@ -15,14 +15,14 @@ class ContactUs extends Model
     public $email;
     public $phone;
     public $call_remember;
-    public $enquiry;
+    public $message;
     public $redirect_url;
 
     public function rules()
     {
         return [
             [['name', 'phone', 'call_remember', 'redirect_url'], 'safe'],
-            [['first_name', 'last_name', 'email', 'enquiry'], 'required'],
+            [['first_name', 'last_name', 'email', 'message'], 'required'],
             ['email', 'email'],
         ];
     }
@@ -32,17 +32,43 @@ class ContactUs extends Model
         $settings = Cms::settings();
         if (isset($settings['general_settings']['admin_email']) && $settings['general_settings']['admin_email'] != '')
         {
-            Yii::$app->mailer->compose('mail', ['model' => $this]) // a view rendering result becomes the message body here
-                    ->setFrom($this->email)
-                    ->setTo($settings['general_settings']['admin_email'])
-                    ->setSubject('Contact')
-                    ->send();
+//            Yii::$app->mailer->compose('mail', ['model' => $this]) // a view rendering result becomes the message body here
+//                    ->setFrom($this->email)
+//                    ->setTo($settings['general_settings']['admin_email'])
+//                    ->setSubject('Contact')
+//                    ->send();
+            $this->saveAccount();
             return true;
         }
         else
         {
             return false;
         }
+    }
+
+    public function saveAccount()
+    {
+        if (!$this->name)
+            $this->name = $this->first_name . ' ' . $this->last_name;
+        $post_items = [];
+        foreach ($this as $key => $value)
+        {
+            $post_items[] = $key . '=' . $value;
+        }
+
+        $post_string = implode('&', $post_items);
+        $post_string .= '&source=web-client';
+
+        $curl_connection = curl_init("https://my.optima-crm.com/yiiapp/frontend/web/index.php?r=accounts/index&user=" . Yii::$app->params['user']);
+        curl_setopt($curl_connection, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($curl_connection, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)");
+        curl_setopt($curl_connection, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl_connection, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl_connection, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl_connection, CURLOPT_POSTFIELDS, $post_string);
+        curl_exec($curl_connection);
+        curl_errno($curl_connection) . '-' . curl_error($curl_connection);
+        curl_close($curl_connection);
     }
 
 }
