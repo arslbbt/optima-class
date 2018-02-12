074e36cc40b90d71fa2db2ff180e66d462f012a7
arsl <arsl@optimageeks.com>
2018-02-13 00:08:17 +0500

    updates'

--- src/models/Cms.php
+++ src/models/Cms.php
@@ -53,7 +53,39 @@ class Cms extends Model
         }
         return json_decode($file_data, TRUE);
     }
-
+    public static function menuYII($id)
+    {
+        $lang = \Yii::$app->language;
+        $webroot = Yii::getAlias('@webroot');
+        if (!is_dir($webroot . '/uploads/'))
+            mkdir($webroot . '/uploads/');
+        if (!is_dir($webroot . '/uploads/temp/'))
+            mkdir($webroot . '/uploads/temp/');
+        $file = $webroot . '/uploads/temp/menu.json';
+        if (!file_exists($file) || (file_exists($file) && time() - filemtime($file) > 2 * 3600))
+        {
+            $file_data = file_get_contents(Yii::$app->params['apiUrl'] . 'cms/menu&user=' . Yii::$app->params['user'] . '&id=' . $id);
+            file_put_contents($file, $file_data);
+        }
+        else
+        {
+            $file_data = file_get_contents($file);
+        }
+        $menu= json_decode($file_data, TRUE);
+    $itemsArr=[];
+    foreach ($menu['menu_items'] as $key => $value) {
+        if(isset($value['children']) && count($value['children'])>0){
+            $childArr=[];
+            foreach ($value['children'] as $childkey => $child) {
+                $childArr[]=['label' => (isset($child['item']['title'][$lang]) && $child['item']['title'][$lang]!='')?$child['item']['title'][$lang]:'please set menu label', 'url' => (isset($child['item']['slug'][$lang]) && $child['item']['slug'][$lang]!='')?$child['item']['slug'][$lang]:'slug-not-set'];
+            }
+            $itemsArr[]=['label' => (isset($value['item']['title'][$lang]) && $value['item']['title'][$lang]!='')?$value['item']['title'][$lang]:'please set menu label', 'items' => $childArr];
+        }else{
+            $itemsArr[]=['label' => (isset($value['item']['title'][$lang]) && $value['item']['title'][$lang]!='')?$value['item']['title'][$lang]:'please set menu label', 'url' => (isset($value['item']['slug'][$lang]) && $value['item']['slug'][$lang]!='')?$value['item']['slug'][$lang]:'slug-not-set'];
+        }
+    }
+    return $itemsArr;
+    }
     public static function languages()
     {
         $webroot = Yii::getAlias('@webroot');
@@ -121,7 +153,21 @@ class Cms extends Model
         {
             $file_data = file_get_contents($file);
         }
-        return json_decode($file_data, TRUE);
+        $dataEach= json_decode($file_data, TRUE);
+        $lang = \Yii::$app->language;
+
+        $retdata=[];
+        $array=[];
+        foreach ($dataEach as $key => $data) {
+             $array['featured_image'] = isset($data['featured_image'][$lang]['name']) ? 'https://my.optima-crm.com/uploads/cms_pages/' . $data['_id'] . '/' . $data['featured_image'][$lang]['name'] : '';
+             $array['content'] = isset($data['content'][$lang]) ? $data['content'][$lang] : '';
+             $array['title'] = isset($data['title'][$lang]) ? $data['title'][$lang] : '';
+             $array['meta_title']=isset($data['meta_title'][$lang]) ? $data['meta_title'][$lang] : '';
+             $array['meta_desc']=isset($data['meta_desc'][$lang]) ? $data['meta_desc'][$lang] : '';
+             $array['meta_keywords']=isset($data['meta_keywords'][$lang]) ? $data['meta_keywords'][$lang] : '';
+        $retdata[]=$array;
+        }
+        return $retdata;
     }
 
 }
