566f75d07afe3056072af63ddc35eb0a7b40d8e7
sirajulhaq <siraj@optimageeks.com>
2018-02-20 10:56:07 +0500

    changing regarding to submenu for sale with locations...

--- src/models/Cms.php
+++ src/models/Cms.php
@@ -142,7 +142,7 @@ class Cms extends Model
             mkdir($webroot . '/uploads/');
         if (!is_dir($webroot . '/uploads/temp/'))
             mkdir($webroot . '/uploads/temp/');
-        $file = $webroot . '/uploads/temp/' . $slug . '.json';
+        $file = $webroot . '/uploads/temp/' . str_replace('/', '_', $slug) . '.json';
         if (!file_exists($file) || (file_exists($file) && time() - filemtime($file) > 2 * 3600))
         {
             $file_data = file_get_contents(Yii::$app->params['apiUrl'] . 'cms/page-by-slug&user=' . Yii::$app->params['user'] . '&slug=' . $slug);
