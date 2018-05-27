7ad1a41b26056cae626f716d06817461c2b0b447
arsl <arsl@optimageeks.com>
2018-05-27 22:17:51 +0500

    up

--- src/models/Properties.php
+++ src/models/Properties.php
@@ -29,8 +29,8 @@ class Properties extends Model
             }
         }
         $query .= self::setQuery();
-        $url = Yii::$app->params['apiUrl'] . 'properties&user=' . Yii::$app->params['user'] . $query;
-        
+        echo $url = Yii::$app->params['apiUrl'] . 'properties&user=' . Yii::$app->params['user'] . $query;
+        die;
         $JsonData = file_get_contents($url);
         $apiData = json_decode($JsonData);
         $settings = Cms::settings();
