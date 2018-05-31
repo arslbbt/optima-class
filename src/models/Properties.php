45a2863b31f52791bb1f42385504d1d84fbe4fe4
Usama Khalil <usama.khalil@optimageeks.com>
2018-05-31 09:13:17 +0500

    booking date rangre

--- src/models/Properties.php
+++ src/models/Properties.php
@@ -1164,12 +1164,12 @@ class Properties extends Model
         if (isset($get["st_date_from"]) && $get["st_date_from"] != "" && $get["st_date_from"] != "Arrival" && isset($get["st_date_from_submit"]) && $get["st_date_from_submit"] != "")
         {
             $stdf = new \DateTime($get["st_date_from_submit"]);
-            $query .= '&st_period_from[]=' . $stdf->getTimestamp();
+            $query .= '&booking_from=' . $stdf->getTimestamp();
         }
         if (isset($get["st_date_to"]) && $get["st_date_to"] != "" && $get["st_date_to"] != "Return" && isset($get["st_date_to_submit"]) && $get["st_date_to_submit"] != "")
         {
             $stdt = new \DateTime($get["st_date_to_submit"]);
-            $query .= '&st_period_to[]=' . $stdt->getTimestamp();
+            $query .= '&booking_to=' . $stdt->getTimestamp();
         }
         if (isset($get["st_from"]) && $get["st_from"] != "")
         {
