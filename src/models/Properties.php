f4f48da6a3020e74011530259bf7bdf57227ec6d
arsl <arsl@optimageeks.com>
2018-07-02 19:54:54 +0500

    update

--- src/models/Properties.php
+++ src/models/Properties.php
@@ -559,7 +559,7 @@ class Properties extends Model
         $seo_title = 'seo_title';
         $seo_description = 'seo_description';
         $keywords = 'keywords';
-        if (isset($property->property->rent) && $property->property->rent == true && !$property->property->sale || $rent==true) 
+        if (isset($property->property->rent) && $property->property->rent == true || $rent==true) 
         {
             $title = 'rental_title';
             $description = 'rental_description';
