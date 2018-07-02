ee874051b3911a4bf2d260ba8badbacb2d88ce8c
arsl <arsl@optimageeks.com>
2018-07-02 20:41:07 +0500

    up

--- src/models/Properties.php
+++ src/models/Properties.php
@@ -553,13 +553,7 @@ class Properties extends Model
         {
             $return_data['reference'] = $property->agency_code . '-' . $property->property->reference;
         }
-        $title = 'title';
-        $description = 'description';
-        $price = 'sale';
-        $seo_title = 'seo_title';
-        $seo_description = 'seo_description';
-        $keywords = 'keywords';
-        if ((isset($property->property->rent) && $property->property->rent == true) || $rent==true) 
+        if ($rent==true) 
         {
             $title = 'rental_title';
             $description = 'rental_description';
@@ -567,17 +561,24 @@ class Properties extends Model
             $seo_title = 'rental_seo_title';
             $seo_description = 'rental_seo_description';
             $keywords = 'rental_keywords';
+            
+        }else{
+            $title = 'title';
+            $description = 'description';
+            $price = 'sale';
+            $seo_title = 'seo_title';
+            $seo_description = 'seo_description';
+            $keywords = 'keywords';
+            if ((isset($property->property->rent) && $property->property->rent == true)) 
+            {
+                $title = 'rental_title';
+                $description = 'rental_description';
+                $price = 'rent';
+                $seo_title = 'rental_seo_title';
+                $seo_description = 'rental_seo_description';
+                $keywords = 'rental_keywords';
+            }
         }
-        // if ($rent==false) 
-        // {
-        //     $title = 'title';
-        //     $description = 'description';
-        //     $price = 'sale';
-        //     $seo_title = 'seo_title';
-        //     $seo_description = 'seo_description';
-        //     $keywords = 'keywords';
-        // }
-
         //    start slug_all
         foreach ($langugesSystem as $lang_sys)
         {
