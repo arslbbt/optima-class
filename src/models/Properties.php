c2d26ff712e374b280463c2b1aa2138d70413a33
OptimaGeeks <optimageeks@OptimaGeekss-MacBook-Pro.local>
2018-07-05 07:04:15 +0200

    last day show for costa.

--- src/models/Properties.php
+++ src/models/Properties.php
@@ -884,7 +884,7 @@ class Properties extends Model
                     }
                     // booking dates for costa - last day available - OPT-3533
                     // Revert above-booking dates for costa - CA search calendar update (OPT-3561)
-                    for ($i = $booking->date_from; $i <= $booking->date_until; $i += 86400)
+                    for ($i = $booking->date_from; $i < $booking->date_until; $i += 86400)
                     {
                         $booked_dates_costa[] = date(isset(Yii::$app->params['date_fromate']) ? Yii::$app->params['date_fromate'] : "m-d-Y", $i);
                     }
