3a2e3cc21865aca35764682b7432b1ac0236d16d
arslbbt <arslbbt@gmail.com>
2019-03-06 15:56:44 +0500

    Update Properties.php
    iset

--- src/models/Properties.php
+++ src/models/Properties.php
@@ -733,7 +733,7 @@ class Properties extends Model
                     $return_data['price'] = ($property->property->currentprice != 0) ? number_format((int)$property->property->currentprice, 0, '', '.') : '';
                 }
             }
-            if (isset($property->property->lt_rental) && $property->property->lt_rental == true && isset($property->property->period_seasons) && is_array($property->property->period_seasons) && $property->property->period_seasons[0]->new_price) {
+            if (isset($property->property->lt_rental) && $property->property->lt_rental == true && isset($property->property->period_seasons) && is_array($property->property->period_seasons) && isset($property->property->period_seasons[0]->new_price)) {
                 $return_data['ltprice'] = ($property->property->period_seasons[0]->new_price != 0) ? number_format((int)$property->property->period_seasons[0]->new_price, 0, '', '.') . ' ' . Yii::t('app', 'per_month') : '';
                 $return_data['lt_price'] = ($property->property->period_seasons[0]->new_price != 0) ? number_format((int)$property->property->period_seasons[0]->new_price, 0, '', '.') . ' € ' . Yii::t('app', 'per_month') : '';
             }
