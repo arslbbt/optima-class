f9ed48841c81d33b88d12cda8f8f5b2b7b65f9e8
arsl <arslbbt@gmail.com>
2019-07-05 14:32:58 +0500

    updates

--- src/models/Functions.php
+++ src/models/Functions.php
@@ -11,9 +11,7 @@ class Functions extends Model
 
     public static function recaptcha($name= 'reCaptcha')
     {
-        $recaptcha_site_key = Yii::$app->params['recaptcha_site_key'];
-        if(empty($recaptcha_site_key)) $recaptcha_site_key = "6Le9fqsUAAAAAN2KL4FQEogpmHZ_GpdJ9TGmYMrT";
-
+        $recaptcha_site_key = isset(Yii::$app->params['recaptcha_site_key'])?Yii::$app->params['recaptcha_site_key']:"6Le9fqsUAAAAAN2KL4FQEogpmHZ_GpdJ9TGmYMrT";
         return \himiklab\yii2\recaptcha\ReCaptcha2::widget([
             'name' => 'reCaptcha',
             'siteKey' => $recaptcha_site_key, // unnecessary is reCaptcha component was set up
