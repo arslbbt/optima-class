1925fb4e9ebec1fe699d64b4558bc5f0b0e8e37a
IMAC 4 <imac4@IMACs-iMac.local>
2019-07-17 15:27:06 +0500

    recaptcha client side check

--- src/models/Functions.php
+++ src/models/Functions.php
@@ -12,11 +12,24 @@ class Functions extends Model
     public static function recaptcha($name= 'reCaptcha')
     {
         $recaptcha_site_key = isset(Yii::$app->params['recaptcha_site_key'])?Yii::$app->params['recaptcha_site_key']:"6Le9fqsUAAAAAN2KL4FQEogpmHZ_GpdJ9TGmYMrT";
-        return \himiklab\yii2\recaptcha\ReCaptcha2::widget([
+        //View::registerJs(' ');
+        $ret = "<script>
+        var form=document.getElementById('recaptcha_validate');
+        form.addEventListener(\"submit\",function(event){
+            if(grecaptcha.getResponse()===''){
+                event.preventDefault();
+                alert('Please check the recaptcha');
+                //document.getElementById('g-recaptcha-error').innerHTML = '<span style=\"color:red;\">This field is required.</span>';
+                return false;
+            }
+        },false);
+        </script>";
+        $ret .= \himiklab\yii2\recaptcha\ReCaptcha2::widget([
             'name' => 'reCaptcha',
             'siteKey' => $recaptcha_site_key, // unnecessary is reCaptcha component was set up
             'widgetOptions' => ['class' => 'col-sm-offset-3'],
         ]);
+        return $ret;
     }
     public static function siteSendEmail($it){
         $model = new ContactUs();
