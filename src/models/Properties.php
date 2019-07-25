2822eaf7d5d0535bcfb43bc49023e1c2e8945b61
arslbbt <arslbbt@gmail.com>
2019-07-25 15:13:19 +0500

    Update Properties.php
                            if (is_numeric($booking->date_from) && is_numaric($booking->date_until)) {
                                for ($i = $booking->date_from; $i <= $booking->date_until; $i += 86400) {
                                    $booked_dates[] = date(isset(Yii::$app->params['date_fromate']) ? Yii::$app->params['date_fromate'] : "m-d-Y", $i);
                                }
                            }

--- src/models/Properties.php
+++ src/models/Properties.php
@@ -998,8 +998,8 @@ class Properties extends Model
                 $booking_status = [];
                 $booked_dates_costa=[];
                 foreach ($property->bookings as $key => $booking) {
-                        if(is_numeric($booking->date_from) && is_numaric($booking->date_until)){
-                        for ($i = $booking->date_from; $i <= $booking->date_until; $i += 86400) {
+                        if (is_numeric($booking->date_from) && is_numaric($booking->date_until)) {
+                            for ($i = $booking->date_from; $i <= $booking->date_until; $i += 86400) {
                                 $booked_dates[] = date(isset(Yii::$app->params['date_fromate']) ? Yii::$app->params['date_fromate'] : "m-d-Y", $i);
                             }
                         }
