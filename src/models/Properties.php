95ed811b218eb911155abecdb3da90247274a011
iMac3 <imac3@iMac3s-iMac.local>
2019-11-12 14:54:06 +0500

    abdullah has implemented this code to display created at

--- src/models/Properties.php
+++ src/models/Properties.php
@@ -420,6 +420,9 @@ class Properties extends Model
                     if (isset($property->property->terrace->{0}->terrace) && $property->property->terrace->{0}->terrace > 0) {
                         $data['terrace'] = $property->property->terrace->{0}->terrace;
                     }
+                    if (isset($property->property->created_at) && !empty($property->property->created_at)) {
+                        $data['created_at'] = $property->property->created_at;
+                    }
                     if (isset($property->property->updated_at) && $property->property->updated_at != '') {
                         $data['updated_at'] = $property->property->updated_at;
                     }
