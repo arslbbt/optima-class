8789f8dd64ab96ecdaad0d87e96866a083439792
iMac3 <imac3@iMac3s-iMac.local>
2019-11-13 12:27:27 +0500

    abdullah requested for this update

--- src/models/Properties.php
+++ src/models/Properties.php
@@ -423,8 +423,8 @@ class Properties extends Model
                     if (isset($property->property->created_at) && !empty($property->property->created_at)) {
                         $data['created_at'] = $property->property->created_at;
                     }
-                    if (isset($property->property->featured) && !empty($property->property->featured)) {
-                        $data['featured'] = $property->property->featured;
+                    if (isset($property->featured) && !empty($property->featured)) {
+                        $data['featured'] = $property->featured;
                     }
                     if (isset($property->property->updated_at) && $property->property->updated_at != '') {
                         $data['updated_at'] = $property->property->updated_at;
@@ -808,8 +808,8 @@ class Properties extends Model
             if (isset($property->property->currentprice) && isset($property->property->sale) && $property->property->sale == true) {
                 $return_data['currentprice'] = $property->property->currentprice;
             }
-            if (isset($property->property->featured) && !empty($property->property->featured)) {
-                $return_data['featured'] = $property->property->featured;
+            if (isset($property->featured) && !empty($property->featured)) {
+                $return_data['featured'] = $property->featured;
             }
             if (isset($property->property->own) && $property->property->own == true) {
                 $return_data['own'] = true;
