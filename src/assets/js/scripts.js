98721d8683d67e891b04d73341177595c866e705
IMAC 4 <imac4@IMACs-iMac-2.local>
2020-01-20 12:14:48 +0500

    translation for selected counter and all selected in dropdowns

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,3 +1,4 @@
 $(document).ready(function () {
-  $('.multiselect').multiselect({ nonSelectedText: $(this).attr("data-placeholder") });
+  // $('.multiselect').multiselect({ nonSelectedText: $(this).attr("data-placeholder") });
+  $('.multiselect').multiselect({ allSelectedText: $('.multiselect').attr("data-allselectedtext"), nSelectedText: $('.multiselect').attr("data-nselectedtext") });
 });
