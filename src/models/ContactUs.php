2183778cbf0890e61608739073130774f8709049
abdullah <abdullah@optimageeks.com>
2020-03-21 07:36:38 +0500

    update

--- src/models/ContactUs.php
+++ src/models/ContactUs.php
@@ -107,10 +107,11 @@ class ContactUs extends Model
     public $cities;
     public $locations;
     public $urbanization;
+    public $furniture;
     public function rules()
     {
         return [
-            [['name', 'mobile_phone', 'phone', 'office', 'call_remember', 'to_email', 'html_content', 'source', 'owner', 'lead_status', 'language', 'parking', 'redirect_url', 'attach', 'reference', 'transaction', 'property_type', 'bedrooms', 'bathrooms', 'pool', 'address', 'house_area', 'plot_area', 'price', 'price_reduced', 'close_to_sea', 'sea_view', 'exclusive_property', 'accept_cookie', 'accept_cookie_text', 'get_updates', 'booking_period', 'guests', 'transaction_types', 'subscribe', 'booking_enquiry', 'sender_first_name', 'sender_last_name', 'sender_email', 'sender_phone', 'assigned_to', 'news_letter', 'arrival_date', 'buy_price_from', 'buy_price_to', 'strent_price_from', 'strent_price_to', 'departure_date', 'contact_check_1', 'contact_check_2', 'contact_check_3', 'resume', 'application', 'cv_file', 'gdpr_status', 'buyer', 'listing_agency_email', 'lgroups', 'feet_setting', 'feet_views', 'sub_types', 'feet_categories', 'p_type', 'year_built_from', 'year_built_to', 'plot_size_from', 'plot_size_to', 'built_size_from', 'built_size_to', 'usefull_area_from', 'usefull_area_to', 'building_style', 'gated_comunity', 'elevator', 'settings', 'orientation', 'views', 'garden', 'only_golf_properties', 'only_off_plan', 'buy_from_date', 'min_purchase_budget', 'max_purchase_budget', 'countries', 'regions', 'provinces', 'cities', 'locations', 'urbanization', 'condition'], 'safe'],
+            [['name', 'mobile_phone', 'phone', 'office', 'call_remember', 'to_email', 'html_content', 'source', 'owner', 'lead_status', 'language', 'parking', 'redirect_url', 'attach', 'reference', 'transaction', 'property_type', 'bedrooms', 'bathrooms', 'pool', 'address', 'house_area', 'plot_area', 'price', 'price_reduced', 'close_to_sea', 'sea_view', 'exclusive_property', 'accept_cookie', 'accept_cookie_text', 'get_updates', 'booking_period', 'guests', 'transaction_types', 'subscribe', 'booking_enquiry', 'sender_first_name', 'sender_last_name', 'sender_email', 'sender_phone', 'assigned_to', 'news_letter', 'arrival_date', 'buy_price_from', 'buy_price_to', 'strent_price_from', 'strent_price_to', 'departure_date', 'contact_check_1', 'contact_check_2', 'contact_check_3', 'resume', 'application', 'cv_file', 'gdpr_status', 'buyer', 'listing_agency_email', 'lgroups', 'feet_setting', 'feet_views', 'sub_types', 'feet_categories', 'p_type', 'year_built_from', 'year_built_to', 'plot_size_from', 'plot_size_to', 'built_size_from', 'built_size_to', 'usefull_area_from', 'usefull_area_to', 'building_style', 'gated_comunity', 'elevator', 'settings', 'orientation', 'views', 'garden', 'only_golf_properties', 'only_off_plan', 'buy_from_date', 'min_purchase_budget', 'max_purchase_budget', 'countries', 'regions', 'provinces', 'cities', 'locations', 'urbanization', 'furniture', 'condition'], 'safe'],
             ['first_name', 'required', 'message' => Yii::t('app', 'first name cannot be blank.')],
             ['last_name', 'required', 'message' => Yii::t('app', 'last name cannot be blank.')],
             ['email', 'required', 'message' => Yii::t('app', 'email cannot be blank.')],
@@ -465,6 +466,7 @@ class ContactUs extends Model
             'orientation' => isset($this->orientation) ? $this->orientation : null,
             'views' => isset($this->views) ? $this->views : null,
             'garden' => isset($this->garden) ? $this->garden : null,
+            'garden' => isset($this->furniture) ? $this->furniture : null,
             'condition' => isset($this->condition) ? $this->condition : null,
             'only_golf_properties' => isset($this->only_golf_properties) ? $this->only_golf_properties : null,
             'only_off_plan' => isset($this->only_off_plan) ? $this->only_off_plan : null,
