c26629b8567efeb00b508153154462ed84cac121
abdullah <abdullah@optimageeks.com>
2020-05-05 15:50:43 +0500

    skip options

--- src/views/partials/selectDropdown.php
+++ src/views/partials/selectDropdown.php
@@ -1,6 +1,6 @@
 
-<?php 
-  $select_html .= '<select 
+<?php
+$select_html .= '<select 
           name="' . (isset($options['name']) ? $options['name'] : '') . '" 
           class="' . (isset($options['class']) ? $options['class'] : '') . '" 
           id="' . (isset($options['id']) ? $options['id'] : '') . '" 
@@ -12,9 +12,11 @@
           ' . (isset($options['multiple']) ? $options['multiple'] : '') . '
           ' . (isset($options['required']) ? $options['required'] : '') . '
           >';
-          
-  foreach ($data as $value) {
-      $select_html .= '<option ' . (is_array(Yii::$app->request->get(str_replace("[]","",$options['name'])))? in_array($value['option_key'], Yii::$app->request->get(str_replace("[]","",$options['name'])))? 'selected' : '' : Yii::$app->request->get(str_replace("[]","",$options['name'])) == $value['option_key']? 'selected' : '' ) . ' value="' . $value['option_key'] . '">' . (isset($options['noValueTranslation']) ? $value['option_value'] : Yii::$app->translate->t($value['option_value'])) . '</option>';
+
+foreach ($data as $value) {
+  if (!in_array($value['option_key'], $options['skip_options'])) {
+    $select_html .= '<option ' . (is_array(Yii::$app->request->get(str_replace("[]", "", $options['name']))) ? in_array($value['option_key'], Yii::$app->request->get(str_replace("[]", "", $options['name']))) ? 'selected' : '' : Yii::$app->request->get(str_replace("[]", "", $options['name'])) == $value['option_key'] ? 'selected' : '') . ' value="' . $value['option_key'] . '">' . (isset($options['noValueTranslation']) ? $value['option_value'] : Yii::$app->translate->t($value['option_value'])) . '</option>';
   }
-  $select_html .= '</select>'; 
+}
+$select_html .= '</select>';
 ?>
