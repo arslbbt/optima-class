d02ddd062cb33fdc3ecced5ecf21b4783632e80c
abdullah <abdullah@optimageeks.com>
2020-05-17 02:58:48 +0500

    Urlhelper component added

---
+++ src/components/Urlhelper.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace optima\components;
+
+use Yii;
+use yii\base\Component;
+use Cocur\Slugify\Slugify;
+use optima\models\Cms;
+
+class Urlhelper extends Component
+{
+
+    public static function slug($tag)
+    {
+        return Cms::getSlugByTagName($tag);
+    }
+
+    public static function propertiesListingUrl()
+    {
+        return self::slug('propertyList');
+    }
+
+    public static function propertyDetailsSlug()
+    {
+        return self::slug('propertyDetails');
+    }
+
+    public static function developmentsListingUrl()
+    {
+        return self::slug('developmentsListing');
+    }
+
+    public static function developmentDetailsSlug()
+    {
+        return self::slug('developmentDetails');
+    }
+
+    public static function blogListingUrl()
+    {
+        return self::slug('blogListing');
+    }
+
+    public static function blogDetailsSlug()
+    {
+        return self::slug('blogDetails');
+    }
+
+    /**
+     * Urlhelper::getPropertyTitle($property, $language)
+     * 
+     * @param mixed $property
+     * @param mixed $language
+     * 
+     * @return string
+     */
+    public static function getPropertyTitle($property, $language = '')
+    {
+        $lang = empty($language) ? strtoupper(Yii::$app->language) : strtoupper($language);
+        $slugify = new Slugify();
+        $permaLink = isset($property['slug_all'][$lang]) ? $property['slug_all'][$lang] : 'property';
+
+        return $slugify->slugify($permaLink) . '_' . $property['id'];
+    }
+
+    /**
+     * Urlhelper::getPropertyUrl($property)
+     * 
+     * @param mixed $property
+     * 
+     * @return url
+     */
+    public static function getPropertyUrl($property)
+    {
+        return '/' . strtolower(Yii::$app->language) . '/' . Urlhelper::propertyDetailsSlug() . '/' . Urlhelper::getPropertyTitle($property);
+    }
+
+    /**
+     * Urlhelper::getDevelopmentTitle($development, $language)
+     * 
+     * @param mixed $development
+     * @param mixed $language
+     * 
+     * @return string
+     */
+    public static function getDevelopmentTitle($development, $language = '')
+    {
+        $lang = empty($language) ? strtoupper(Yii::$app->language) : strtoupper($language);
+        $slugify = new Slugify();
+        $permaLink = isset($development['slug_all'][$lang]) ? $development['slug_all'][$lang] : 'property';
+
+        return $slugify->slugify($permaLink) . '_' . $development['id'];
+    }
+
+    /**
+     * Urlhelper::getDevelopmentUrl($development, $language)
+     * 
+     * @param mixed $development
+     * 
+     * @return url
+     */
+    public static function getDevelopmentUrl($development)
+    {
+        return '/' . strtolower(Yii::$app->language) . '/' . Urlhelper::developmentDetailsSlug() . '/' . Urlhelper::getDevelopmentTitle($development);
+    }
+
+    /**
+     * Urlhelper::getPostTitle($post, $language)
+     * 
+     * @param mixed $post
+     * @param mixed $language
+     * 
+     * @return string
+     */
+    public static function getPostTitle($post, $language = '')
+    {
+        $lang = empty($language) ? strtoupper(Yii::$app->language) : strtoupper($language);
+        $slugify = new Slugify();
+        $postSlug = isset($post['slug_all'][$lang]) ? $post['slug_all'][$lang] : 'post-not-found';
+
+        return $slugify->slugify($postSlug);
+    }
+
+    /**
+     * Urlhelper::getBlogUrl($development, $language)
+     * 
+     * @param mixed $post
+     * @param string $language
+     * 
+     * @return url
+     */
+    public static function getBlogUrl($post)
+    {
+        return '/' . strtolower(Yii::$app->language) . '/' . Urlhelper::blogDetailsSlug() . '/' . Urlhelper::getPostTitle($post);
+    }
+}
