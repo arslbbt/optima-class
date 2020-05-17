41fdefca528d078b6dfcb716f46d5e2bc84e2d92
abdullah <abdullah@optimageeks.com>
2020-05-17 17:53:53 +0500

    ErrorAction for Error reporting

---
+++ src/actions/OptimaErrorAction.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace optima\actions;
+
+use Yii;
+use yii\web\ErrorAction;
+
+class OptimaErrorAction extends ErrorAction
+{
+    /**
+     * Runs the action.
+     *
+     * @return string result content
+     */
+    public function run()
+    {
+        if (!YII_DEBUG) {
+            return $this->controller->redirect(array('/404', 'language' => strtolower(Yii::$app->language)));
+        }
+
+        if ($this->layout !== null) {
+            $this->controller->layout = $this->layout;
+        }
+
+        Yii::$app->getResponse()->setStatusCodeByException($this->exception);
+
+        if (Yii::$app->getRequest()->getIsAjax()) {
+            return $this->renderAjaxResponse();
+        }
+
+        return $this->renderHtmlResponse();
+    }
+}
