0643bd40541d0c507da0fede3520c1356c1baea6
abubakkar <abubakar@optimageeks.com>
2021-08-25 10:36:03 +0500

    blank file not create on skip cache

--- src/models/Cms.php
+++ src/models/Cms.php
@@ -495,25 +495,29 @@ class Cms extends Model
      */
     public static function getSlugs($params = [])
     {
+        $file = '';
         $name = isset($params['name']) ? $params['name'] : 'all';
         $with_templates = isset($params['without_templates']) ? false : true;
         $with_tags = isset($params['without_tags']) ? false : true;
-        $file = Functions::directory() . 'slugs_for_';
-        $file .= str_replace(' ', '_', strtolower($name));
-        $file .= $with_templates ? '_with_templates' : '';
-        $file .= $with_tags ? '_with_tags' : '';
-        $file .= '.json';
+        
+        $query = isset(\Yii::$app->params['user']) ? '&user=' . \Yii::$app->params['user'] : '';
+        $query .= isset(\Yii::$app->params['site_id']) ? '&site_id=' . \Yii::$app->params['site_id'] : '';
+        $query .= $with_templates ? '&expand=template' : '';
+        $query .= $with_tags ? '&tags=true' : '';
+        
+        $url = Yii::$app->params['apiUrl'] . 'cms/get-slugs-v2' . $query;
+        $file_data = Functions::getCRMData($url);
 
+        if ($file_data != '[]') {
+            $file = Functions::directory() . 'slugs_for_';
+            $file .= str_replace(' ', '_', strtolower($name));
+            $file .= $with_templates ? '_with_templates' : '';
+            $file .= $with_tags ? '_with_tags' : '';
+            $file .= '.json';
+        }
         if (!file_exists($file) || (file_exists($file) && time() - filemtime($file) > 2 * 3600)) {
-            $query = isset(\Yii::$app->params['user']) ? '&user=' . \Yii::$app->params['user'] : '';
-            $query .= isset(\Yii::$app->params['site_id']) ? '&site_id=' . \Yii::$app->params['site_id'] : '';
-            $query .= $with_templates ? '&expand=template' : '';
-            $query .= $with_tags ? '&tags=true' : '';
-
-            $url = Yii::$app->params['apiUrl'] . 'cms/get-slugs-v2' . $query;
-
-            $file_data = Functions::getCRMData($url);
-            if ($file_data)
+            
+            if ($file_data !='[]')
                 file_put_contents($file, $file_data);
         } else
             $file_data = file_get_contents($file);
