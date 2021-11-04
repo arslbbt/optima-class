a2189e68cb2814d318069c5b29d7bf08a39ee159
abubakkar <abubakar@optimageeks.com>
2021-11-04 14:59:08 +0500

    get provinces for all coutries

--- src/models/Dropdowns.php
+++ src/models/Dropdowns.php
@@ -67,7 +67,7 @@ class Dropdowns extends Model
     // use Dropdowns::getProvinces() as it will provide more options to handle data in controller and works with countries and regions search too
     public static function provinces($country = '')
     {
-        $country_query = '&country=' . $country;
+        $country_query = $country == 'all' ? '&country=all' : '';
         $file = Functions::directory() . 'provinces.json';
         if (!file_exists($file) || (file_exists($file) && time() - filemtime($file) > 2 * 3600)) {
             $url = Yii::$app->params['apiUrl'] . 'properties/provinces&user_apikey=' . Yii::$app->params['api_key'] . $country_query;
